de9ee064113306f8a91af22031a2f2583d8d696a
Theo Armour <t.armour@gmail.com>
2013-05-14 11:12:25 -0700

    reorg home. book summary, list.js

--- list.js
+++ list.js
@@ -1,6 +1,6 @@
 var list = {
-	'Summary': {
-		'Overview': [
+	'Overview': {
+		'': [
 			[ 'Home Page','1-overview/overview/1Home-Page' ],
 			[ 'Read Me','1-overview/overview/0readme' ],
 			[ 'Book Summary','1-overview/overview/2Book-Summary' ],
@@ -8,10 +8,12 @@ var list = {
 			[ 'Credits & Timeline','1-overview/overview/4Credits' ],
 			[ 'Copyright & License','1-overview/overview/5License' ]
 		],
+	},	
+	'Summary': {	
 		'Part 1 ~ Outlining the Problem': [
 			
 			[ 'Part 1 Summary','2-summary/1part/000part1' ],
-			[ 'Chapter 0 ~ Absence','2-summary/1part/00absence' ],
+			[ '<br>Chapter 0 ~ Absence','2-summary/1part/00absence' ],
 			[ 'Chapter 1 ~ (W)holes','2-summary/1part/01w-holes' ],
 			[ 'Chapter 2 ~ Homunculi','2-summary/1part/02homunculi' ],
 			[ 'Chapter 3 ~ Golems','2-summary/1part/03golems' ],
@@ -20,7 +22,7 @@ var list = {
 		],
 		'Part 2 ~ Outlining an Alternative Theory': [ 
 			[ 'Part 2 Summary','2-summary/2part/00part2' ],
-			[ 'Chapter 6 ~ Constraint','2-summary/2part/06constraint' ],
+			[ '<br>Chapter 6 ~ Constraint','2-summary/2part/06constraint' ],
 			[ 'Chapter 7 ~ Homeodynamics','2-summary/2part/07homeodynamics' ],
 			[ 'Chapter 8 ~ Morphodynamics','2-summary/2part/08morphodynamics' ],
 			[ 'Chapter 9 ~ Teleodynamics','2-summary/2part/09teleodynamics' ],
@@ -28,17 +30,21 @@ var list = {
 		],
 		'Part 3 ~ Exploring the Implications': [       
 			[ 'Part 3 Summary','2-summary/3part/00part3' ],
-			[ 'Chapter 11 ~ Work','2-summary/3part/11work' ],
+			[ '<br>Chapter 11 ~ Work','2-summary/3part/11work' ],
 			[ 'Chapter 12 ~ Information','2-summary/3part/12information' ],
 			[ 'Chapter 13 ~ Significance','2-summary/3part/13significance' ],
 			[ 'Chapter 14 ~ Evolution','2-summary/3part/14evolution' ],
 			[ 'Chapter 15 ~ Self','2-summary/3part/15self' ],
 			[ 'Chapter 16 ~ Sentience','2-summary/3part/16sentience' ],
 			[ 'Chapter 17 ~ Consciousness','2-summary/3part/17consciousness' ],
-			[ 'Epilogue','2-summary/3part/18epilogue' ]
+			[ '<br>Epilogue','2-summary/3part/18epilogue' ]
 		],
-		'Explanations': [
-			['Read Me', '3-explanations/0-readme/Read-Me'],
+	},
+	'Index & Glossary': {	
+		'Expanded Index': [
+			['Topics', '3-explanations/0-readme/Read-Me'],
+		],	
+		'Expanded Glossary': [	
 			['Absential', '3-explanations/absential/absential'],
 			['Autogen', '3-explanations/autogen/autogen'],
 			['Constraints', '3-explanations/constraints/constraints'],
